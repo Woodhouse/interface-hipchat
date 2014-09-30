aa6f0a6fdbf40ccfaafdd06ab8051aa803ad9194
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-09-30 14:17:35 +0100

    Fixed issue with it declaring itself always as woodhouse bot

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ module.exports = function(){
     this.joinRoom = function(room){
         var packet, x;
         packet = new xmpp.Element("presence", {
-          to: "" + room + "/" + "Woodhouse Bot"
+          to: "" + room + "/" + this.botname;
         });
         x = packet.c("x", {
           xmlns: "http://jabber.org/protocol/muc"
